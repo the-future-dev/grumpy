8939d5ec652610ccba8c4e866ae0c0b7c5db991a
maltesundgren <malte.sundgren@gmail.com>
2025-02-10 13:30:14 +0100

    Had problems with launch command so I had to redo the package containing the launch files so that it did not lead with the name launch. Now the launch works but there is problems with the launch files

---
+++ project/robot_launch/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'robot_launch'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append((os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Malte Sundgren',
+    maintainer_email='maltesu@kth.se',
+    description='',
+    license='TODO: License declaration',
+    tests_require=[],
+    entry_points={},
+)
