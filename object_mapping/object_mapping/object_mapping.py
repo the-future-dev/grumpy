39652b705ac447ac8df3ab0f586a39e70fa23415
Malte <malte.sundgren@gmail.com>
2025-02-11 16:26:00 +0100

    Changed it to integrate with the detection node from perception

--- object_mapping/object_mapping/object_mapping.py
+++ object_mapping/object_mapping/object_mapping.py
@@ -35,7 +35,7 @@ class ObjectMapping(Node):
         self.margin = 0.1
 
         # Intialize map file
-        self.filepath = '/home/robot/dd2419_ws/object_map.txt'
+        self.filepath = 'object_map.txt'
         with open(self.filepath, 'w') as file:
             pass # creates empty file
 
@@ -46,70 +46,45 @@ class ObjectMapping(Node):
         # Initialize the transform broadcaster
         self.tf_broadcaster = TransformBroadcaster(self)
         
-         # Subscribe to point cloud topic and call callback function on each received message
-        self.create_subscription(PointCloud2, '/camera/depth/color/ds_points', self.detection_callback, 10)
-
-    def detection_callback(self, msg: PointCloud2):
+        # Initialize the subscriber to the object poses 
+        self.create_subscription(Pose, '/object_poses', self.object_pose_callback, 10)
+        
+    
+    def object_pose_callback(self, msg: Pose):
         """
-        Callback that when a pointcloud of an object is published it writes the position from the transform published at the same time into a text file.
+        Writes the object poses and removes (or at least tries) duplicates
         """
-        for object_name in ['Red_Sphere', 'Green_Cube']:
+        label = 'L'
+        x = float(round(msg.position.x, 2))
+        y = float(round(msg.position.y, 2))
+        angle = 0
+        
+        #if object list is empty add the first entry otherwise look for duplicates
+        if not self.object_list:
+            self.object_list.append((label,x,y,angle))
 
-            from_frame_rel = 'odom'
-            to_frame_rel = object_name
-            time = rclpy.time.Time().from_msg(msg.header.stamp)
+            with open(self.filepath, 'a') as file:
+                file.write(f"{label} {x} {y} {angle}\n")
+        
+        else:
+            # Go over current objects to not add duplicates
+            duplicate = False
+            for obj in self.object_list:
+                _,x_obj,y_obj,_ = obj
 
-            # Wait for the transform asynchronously
-            tf_future = self.tf_buffer.wait_for_transform_async(
-                target_frame=to_frame_rel,
-                source_frame=from_frame_rel,
-                time=time
-            )
+                if math.hypot(x-x_obj, y-y_obj) < self.margin:
+                    duplicate = True
 
-            # Spin until transform found or `timeout_sec` seconds has passed
-            rclpy.spin_until_future_complete(self, tf_future, timeout_sec=1)
-
-            # lookup transform between object and odom
-            try:
-                t = self.tf_buffer.lookup_transform(
-                    to_frame_rel,
-                    from_frame_rel,
-                    time)          
-            except TransformException as ex:
-                self.get_logger().info(
-                    f'Could not transform {to_frame_rel} to {from_frame_rel}: {ex}')
-                continue
-            
-            # set parameters for potential new object or box
-            label = 'R' if object_name=='Red_Sphere' else 'G'
-            x = float(round(t.transform.translation.x, 2))
-            y = float(round(t.transform.translation.y, 2))
-            angle = 0
-
-            # if object list is empty add the first entry otherwise look for duplicates
-            if not self.object_list:
-                self.object_list.append((label,x,y,angle))
-                with open(self.filepath, 'a') as file:
-                    file.write(f"{label} {x} {y} {angle}\n")
-            
-            else:
-                # Go over current objects to not add duplicates
-                coordinates_list = []
-                for obj in self.object_list:
-                    _,x_obj,y_obj,_ = obj
-                    coordinates_list.append((x_obj,y_obj))
-
-                coordinates_array = np.array(coordinates_list)
-
-                differences = np.abs(coordinates_array - (x,y))
-                matches = np.all(differences <= self.margin, axis=1)
-                if np.any(matches):
-                    continue
-
+                
+            if not duplicate:
                 # If new object is further away than the margin it is added in the map file and the object list
                 self.object_list.append((label,x,y,angle))
                 with open(self.filepath, 'a') as file:
                     file.write(f"{label} {x} {y} {angle}\n")
+
+
+                
+
 
 
 def main():
