3a1a23e46adaa6eba456112b33b83488a6915cb0
Andrea Ritossa - COMPUTER GROUP5 <andre.ritossa@gmail.com>
2025-02-21 11:26:17 +0100

    improved ground removal & logic for saving objects locally

--- perception_pkg/perception_pkg/detection_node.py
+++ perception_pkg/perception_pkg/detection_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import numpy as np
 
@@ -11,17 +11,21 @@ from rclpy.node import Node
 
 from grumpy_interfaces.msg import ObjectDetection1D 
 
-from tf2_ros import TransformException, TransformBroadcaster
+from visualization_msgs.msg import Marker
+
+from tf2_ros import TransformBroadcaster
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
-from geometry_msgs.msg import Pose, TransformStamped
-import tf2_geometry_msgs
-
-# from perception_pkg.msg import ObjectDetection1D
-# from msg import LabelPointStamped
+from geometry_msgs.msg import Pose
 
 import math
 from sklearn.cluster import DBSCAN
+from scipy.spatial import cKDTree
+# from sklearn.decomposition import PCA
+
+
+import ctypes
+import struct
 
 class Detection(Node):
 
@@ -31,9 +35,8 @@ class Detection(Node):
         self._pub = self.create_publisher(
             PointCloud2, '/perception/camera/ground_filter', 10)
 
-        self._pub_detected_objects = self.create_publisher(
-            PointCloud2, '/perception/detected_objects', 10)
-        
+        self.marker_publisher = self.create_publisher(Marker, '/perception/object_names_marker', 10)
+
         self._object_pose = self.create_publisher(ObjectDetection1D, '/perception/object_poses', 10)
 
         # Subscribe to point cloud topic and call callback function on each received message
@@ -47,155 +50,113 @@ class Detection(Node):
 
         # self.static_transform = self.get_static_transform()
         self.get_logger().info(f"COMPLETED: Node initialization")
-    
+
+        self.object_data_list = []
+
     def cloud_callback(self, msg: PointCloud2):
         # Convert ROS -> NumPy
         gen = pc2.read_points_numpy(msg, skip_nans=True)
         points = np.asarray(gen[:, :3])
-        # print(f"Esempio punto: {points[0]}")
-        colors = gen[:, 3]
-        self.get_logger().debug(f"COMPLETED: PointCloud to Numpy")
+        colors_rgb = np.empty(points.shape, dtype=np.uint32)
+
+        for idx, x in enumerate(gen):
+            c = x[3]
+            s = struct.pack('>f', c)
+            i = struct.unpack('>l', s)[0]
+            pack = ctypes.c_uint32(i).value
+            colors_rgb[idx, 0] = np.asarray((pack >> 16) & 255, dtype=np.uint8)
+            colors_rgb[idx, 1] = np.asarray((pack >> 8) & 255, dtype=np.uint8)
+            colors_rgb[idx, 2] = np.asarray(pack & 255, dtype=np.uint8)
+
+        colors_rgb = colors_rgb.astype(np.float32) / 255
 
         # GROUND REMOVAL #######################################################################
-        # Distance filter: approximate implementation as no transform
-        # Transformation from camera_depth_optical_frame to base_link for correct x,y,z
+        # Transform from camera_depth_optical_frame to base_link
         points = self.fast_transform2(points)
-            # fast_transformed_points = np.asarray(self.fast_transform(points))
-        distances = np.linalg.norm(points, axis=1)
-        distance_rel_indexes = np.where(distances <= 0.9)[0]
 
-        gen = gen[distance_rel_indexes, :]
-        points = points[distance_rel_indexes, :]
+        # Analyze relevant points
+        #  - 2D disance less than 1.5 meters
+        #  - z-axis height less than 20 cm TODO: analyze in "production" (1) obstacles? (2) human noise
+        distance_relevance = np.sqrt(points[:, 0]**2 + points[:, 1]**2) < 1.5
+        height_relevance = (points[:, 2] <= 0.2) & (points[:, 2] >= 0.010)
         
-        # Ground and heigh filter
-        z_values = np.asarray(points)[:, 2]
-        relevant_mask = (np.abs(z_values) <= 0.2) & (z_values > 0.012)  
+        relevant_mask = distance_relevance & height_relevance
         relevant_indices = np.where(relevant_mask)[0]
-
         gen = gen[relevant_indices, :]
         points = points[relevant_indices, :]
+        colors_rgb = colors_rgb[relevant_indices, :]
 
-        self.get_logger().debug(f"COMPLETED: Filter for distance and height")
-        
-        msg_object_detected = pc2.create_cloud(msg.header, msg.fields, gen)
-        self._pub.publish(msg_object_detected)
+        # Ground filtering 
+        # - slope based method
+        # - static cut of points with z between [0.010, 0.2] (for efficiency)
+        ground_inds, non_ground_inds = self.slope_based_ground_filter(points[:, 0], points[:, 1], points[:, 2])
+        non_ground_mask = ~np.isin(np.arange(len(points)), ground_inds, invert=False)
+        gen = gen[non_ground_mask]
+        points = points[non_ground_mask]
+        colors_rgb = colors_rgb[non_ground_mask]
 
-        # Point Clustering #######################################################################
+        msgs_pub = pc2.create_cloud(msg.header, msg.fields, gen)
+        self._pub.publish(msgs_pub) # TODO: remove debug
+
         if points.size == 0:
             self.get_logger().debug("No relevant points detected after filtering.")
-            # Publish an empty pointcloud or handle it as needed
-            empty_cloud = pc2.create_cloud(msg.header, msg.fields, [])
-            self._pub_detected_objects.publish(empty_cloud)
             return
-         
-        filtered_points = points # self.voxel_grid_filter(points, leaf_size=0.05)
-        db = DBSCAN(eps=0.3, min_samples=60)
-        labels = db.fit_predict(filtered_points)
-        self.get_logger().debug(f"COMPLETED: Point Cluster")
         
-        detected_objects = []
-        classified_labels = []
-        unique_labels = set(labels)
+        # Point Clustering #######################################################################
+        db = DBSCAN(eps=0.25, min_samples=60)
+        labels = db.fit_predict(points)
         
-        if len(unique_labels) > 0:
-            for label in unique_labels:
-                if label == -1: # Ignore noise
-                    continue
-                
-                cluster_points = filtered_points[labels == label]
+        for label in set(labels):
+            if label == -1:  # Ignore noise
+                continue
 
-                if cluster_points.shape[0] < 10:
-                    self.get_logger().info(f"Neglecting small cluster / {cluster_points.shape[0]}")
-                    continue
+            cluster_indices = np.where(labels == label)[0]
+            cluster_points = points[cluster_indices]
+            cluster_rgb = colors_rgb[cluster_indices]
 
-                # Compute Bounding Box (More Robust)
-                # 1. Find min/max along each axis
-                min_x = np.min(cluster_points[:, 0])
-                max_x = np.max(cluster_points[:, 0])
-                min_y = np.min(cluster_points[:, 1])
-                max_y = np.max(cluster_points[:, 1])
-                min_z = np.min(cluster_points[:, 2])
-                max_z = np.max(cluster_points[:, 2])
+            if cluster_points.size == 0:
+                continue
+            
+            # SAVE #################################################################################
+            cluster = np.concatenate((cluster_points, cluster_rgb), axis=1)
+            self.object_data_list.append(cluster)
+            self.save_object_data()
 
-                bbox_min = np.array([min_x, min_y, min_z])
-                bbox_max = np.array([max_x, max_y, max_z])
-                bbox_size = bbox_max - bbox_min
-                bbox_size_cm = bbox_size * 100 
+            # Object Detection and Classification ##################################################
+            object_data = self.classify_object(cluster_points, cluster_rgb)
+            if object_data:
+                x_obj, y_obj, z_obj = object_data['centroid']
+                object_label = object_data['label']
 
-                # Classify Objects
-                def debug_detection_print(obj_name, label, bbox_size_cm, volume, position):
-                    return f"detected: {label} | {obj_name} | {bbox_size_cm} | {volume} | {position} "
+                s = ObjectDetection1D()
+                s.header.stamp = msg.header.stamp
+                s.header.frame_id = "base_link"
+                s.label.data = 'B' if object_label == 'Box' else 'O'
+                s.pose.position.x = x_obj
+                s.pose.position.y = y_obj
+                s.pose.position.z = z_obj
 
-                if (bbox_size_cm[0] + bbox_size_cm[1]) >= 5.0 and bbox_size_cm[2] >= 1.5:
-                    # Classify Objects (using volume as before, but you could also use area if appropriate)
-                    volume = np.prod(bbox_size_cm)  # Use cm for volume calculation
-
-                    if volume < 1000:
-                        self.get_logger().info(
-                            debug_detection_print("Object", label, bbox_size_cm, volume, np.mean(cluster_points, axis=0))
-                        )
-                        classified_labels.append("Object")
-                    elif volume < 3720:  # Larger object (Box)
-                        self.get_logger().info(
-                            debug_detection_print("Box", label, bbox_size_cm, volume, np.mean(cluster_points, axis=0))
-                        )
-                        classified_labels.append("Box")
-                    detected_objects.append(cluster_points)
-                else:
-                    self.get_logger().info(
-                        f"Label {label} Neglecting small cluster {bbox_size_cm}")
+                self._object_pose.publish(s)
+                self.place_object_frame((x_obj, y_obj, z_obj), "base_link", msg.header.stamp, f"{label}| {object_label}")  # Use label and object_label
             else:
-                    self.get_logger().info("No labels found, no clusters to process.")
-
-        # # OBJ Pubblication #######################################################################
-        # # Send detected pointcloud
-        # # msg_object_detected = pc2.create_cloud(msg.header, msg.fields, gen)
-        # # self._pub.publish(msg_object_detected)
-        # clusters = [(1,1,1), (1,1,1), (1,1,1)]
-        # all_detected_points = np.concatenate(detected_objects, axis=0) if detected_objects else np.array([])
-        # msg_detected_objects = pc2.create_cloud(pointcloud_header, msg.fields, all_detected_points)
-        # self._pub_detected_objects.publish(msg_detected_objects)
-        
-
-        for i, (cluster, label) in enumerate(zip(detected_objects, classified_labels)):
-            x_points = []
-            y_points = []
-            z_points = []
-            for point in cluster:
-                x,y,z = point
-                x_points.append(x)
-                y_points.append(y)
-                z_points.append(z)
-            
-            x_obj = np.mean(x_points)
-            y_obj = np.mean(y_points)
-            z_obj = np.mean(z_points)
-            
-           
-            s = ObjectDetection1D()
-            s.header.stamp = msg.header.stamp
-            s.header.frame_id = "base_link"
-            s.label.data = 'B' if 'Box' in label else 'O'
-            s.pose.position.x = x_obj
-            s.pose.position.y = y_obj
-            s.pose.position.z = z_obj
-
-            self._object_pose.publish(s)
-            self.place_object_frame((x_obj, y_obj, z_obj), "base_link", msg.header.stamp, f"{i}| {label}")
-
-        #     msg_object_detected = LabelPointStamped()
-        #     msg_object_detected.label.data = "Ciao"
-        #     ps = PoseStamped()
-        #     ps.position.x = x_obj
-        #     ps.position.y = y_obj
-        #     ps.position.z = z_obj
-        #     ps.header.stamp = msg.header.stamp
-        #     ps.header.frame_id = "base_link"
-        #     msg_object_detected.pose
-        #     self._pub_objs.publish(msg_object_detected)
+                self.get_logger().info(f"Label {label} Neglecting potential cluster")
     
+    def save_object_data(self):
+        """Saves all collected object data to a .npz file."""
+        data_save_path = "object_data"
+        # Prepare a dictionary to save, where keys are like 'cluster_0', 'cluster_1', etc.
+        arrays_to_save = {}
+        for i, item in enumerate(self.object_data_list):
+            arrays_to_save[f'cluster_{i}'] = item
+
+        np.savez_compressed(data_save_path, **arrays_to_save)
+        print(f"Object data saved to: {data_save_path}")
+        
+        self.get_logger().info(f"Saved object data to object_data.npz")
+
+
     def fast_transform2(self, points):
-        """Static Transform from ´´ to ´base_link´"""
+        """Static Transform from ´camera_depth_optical_frame´ to ´base_link´"""
         translation = np.array([0.08987, 0.0175, 0.10456])
         rotation = np.array([-0.5, 0.5, -0.5, 0.5])
 
@@ -235,55 +196,122 @@ class Detection(Node):
             downsampled_points.append(centroid)
         return np.array(downsampled_points)
 
+    def slope_based_ground_filter(self, x, y, z):
+        """
+        Filters ground points from LiDAR data using a slope-based method.
+
+        Args:
+            x, y, z: three 1D numpy array of x-coordinates.
+
+        Returns:
+            ground_indices: Indices of points classified as ground.
+            non_ground_indices: Indices of points classified as non-ground.
+        """
+        SLOPE_THRESHOLD = 100
+        K = 3
+
+        num_points = len(x)
+        ground_indices = []
+        non_ground_indices = []
+
+        if len(x) <= K:
+            if num_points == 0:
+                return np.array([]), np.array([])
+            return np.array([]), np.arange(num_points)
+
+        tree = cKDTree(np.column_stack((x, y)))
+
+        # Query for the k-nearest neighbors
+        distances, neighbor_indices = tree.query(np.column_stack((x, y)), k=K + 1)
+        neighbor_indices = neighbor_indices[:, 1:]  # Exclude self
+
+        # Get difference in z-coordinates of the neighbors
+        delta_z = z[neighbor_indices] - z[:, np.newaxis]  # Shape: (num_points, k)
+        # Calculate the distances in the x-y plane (vectorized)
+        delta_xy = np.sqrt((x[neighbor_indices] - x[:, np.newaxis])**2 + (y[neighbor_indices] - y[:, np.newaxis])**2)
+        
+        # Handle cases where delta_xy is zero to avoid division by zero.
+        delta_xy[delta_xy == 0] = 1e-6 # Or another very small value
+
+        # Calculate slopes (vectorized)
+        slopes = np.abs(delta_z / delta_xy)  # Shape: (num_points, k)
+
+        # Find the maximum slope for each point
+        max_slopes = np.max(slopes, axis=1)  # Shape: (num_points,)
+
+        # Identify ground points based on the slope threshold
+        ground_mask = max_slopes <= SLOPE_THRESHOLD
+        ground_indices = np.where(ground_mask)[0]
+        non_ground_indices = np.where(~ground_mask)[0]
+
+        return ground_indices, non_ground_indices
+
+
+    def classify_object(self, cluster_points, cluster_rgb):
+        """Classifies a point cloud cluster into an object type."""
+
+        bbox_min = np.min(cluster_points, axis=0)
+        bbox_max = np.max(cluster_points, axis=0)
+        bbox_size = bbox_max - bbox_min
+        bbox_size_cm = bbox_size * 100
+        volume = np.prod(bbox_size_cm)
+
+        centroid = np.mean(cluster_points, axis=0)
+
+        return {'label': f"OBJ", 'centroid': centroid, 'bbox_min': bbox_min, 'bbox_max': bbox_max, 'volume': volume} 
+
+        # if (bbox_size_cm[0] + bbox_size_cm[1]) >= 5.0 and bbox_size_cm[2] >= 1.5:  # Size threshold (tune this)
+        #     if volume < 1000:
+        #         object_label = "Object"
+        #     elif volume < 3720:
+        #         object_label = "Box"
+        #     else:  # Handle cases where the object is larger than the box threshold
+        #         object_label = "Large Object"
+        #         self.get_logger().info(f"Large Object detected: {bbox_size_cm} | {volume}")
+            
+        #     self.get_logger().info(f"detected: {object_label} | Box size: {bbox_size_cm} | Volume: {volume} | Cluster point mean: {centroid}")
+
+        #     return {'label': object_label, 'centroid': centroid, 'bbox_min': bbox_min, 'bbox_max': bbox_max}  # Return object data
+
+        # else:
+        #     self.get_logger().info(f"Neglecting small cluster {bbox_size_cm}")
+        #     return None
+
     def place_object_frame(self, point, frame_id, stamp, object_name):
         """Take points that correspond to an object and place a frame for it in RViz"""
         x, y, z = point
 
-        # crete pose from these mean values
         pose = Pose()
         pose.position.x = x
         pose.position.y = y
         pose.position.z = z
         
-        # Now we want to express the pose in the map frame, hence we get the transform between the frames
-        to_frame_rel = 'odom'
-        from_frame_rel = frame_id
-        time = rclpy.time.Time().from_msg(stamp)
+        marker = Marker()
+        marker.header.frame_id = 'base_link' # Same frame as your transform
+        marker.header.stamp = stamp
+        marker.ns = "object_names"  # Namespace for your markers (important!)
+        marker.id = id(object_name) # Unique ID for each object (use a counter or hash)
+        marker.type = Marker.TEXT_VIEW_FACING
+        marker.action = Marker.ADD
+        marker.pose.position.x = x  # Same position as your transform
+        marker.pose.position.y = y  # Same position as your transform
+        marker.pose.position.z = z  # Same position as your transform
+        marker.pose.orientation.x = 0.0 # Orientation for text
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
+        marker.scale.x = 0.1  # Adjust text size
+        marker.scale.y = 0.1
+        marker.scale.z = 0.1
+        marker.color.r = 1.0  # Text color (red in this example)
+        marker.color.g = 1.0
+        marker.color.b = 1.0
+        marker.color.a = 1.0  # Alpha (transparency)
+        marker.text = object_name
+        marker_lifetime = rclpy.duration.Duration(seconds=1.0).to_msg()
+        marker.lifetime = marker_lifetime
 
-        # Wait for the transform asynchronously
-        tf_future = self.tf_buffer.wait_for_transform_async(
-            target_frame=to_frame_rel,
-            source_frame=from_frame_rel,
-            time=time
-        )
-
-        # Spin until transform found or `timeout_sec` seconds has passed
-        rclpy.spin_until_future_complete(self, tf_future, timeout_sec=1)
-
-        # lookup transform from frame id to map 
-        try:
-            t_map_cam = self.tf_buffer.lookup_transform(to_frame_rel, from_frame_rel, time)
-        except TransformException as ex:
-            self.get_logger().info(
-                f'Could not transform {to_frame_rel} to {from_frame_rel}: {ex}')
-            return
-        
-        # transform the marker pose so that it is expressed in map coordinates 
-        pose = tf2_geometry_msgs.do_transform_pose(pose, t_map_cam)
-
-        t = TransformStamped()
-        t.header.stamp = stamp
-        t.header.frame_id = 'odom'
-        t.child_frame_id = object_name
-        t.transform.translation.x = pose.position.x
-        t.transform.translation.y = pose.position.y
-        t.transform.translation.z = pose.position.z
-        t.transform.rotation.x = pose.orientation.x
-        t.transform.rotation.y = pose.orientation.y
-        t.transform.rotation.z = pose.orientation.z
-        t.transform.rotation.w = pose.orientation.w
-
-        self.tf_broadcaster.sendTransform(t)
+        self.marker_publisher.publish(marker)
 
 def main():
     rclpy.init()
