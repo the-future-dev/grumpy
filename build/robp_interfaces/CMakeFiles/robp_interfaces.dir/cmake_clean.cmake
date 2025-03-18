c0038d96294004ad7e9d6e967fc4243249fd9d5a
phlinde <phlinde@kth.se>
2025-03-18 14:59:37 +0100

    Removed unnecessary files

--- build/robp_interfaces/CMakeFiles/robp_interfaces.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/robp_interfaces"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/robp_interfaces.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/robp_interfaces/ament_cmake_python/robp_interfaces/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='robp_interfaces',
-    version='1.0.0',
-    packages=find_packages(
-        include=('robp_interfaces', 'robp_interfaces.*')),
-)
