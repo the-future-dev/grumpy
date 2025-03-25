f0045bd27d1481523dd637615269ae6eb33fe230
Andrea Ritossa - COMPUTER GROUP5 <andre.ritossa@gmail.com>
2025-03-25 17:28:06 +0100

    Detection Node towards milestone 3: good single object detection, quite resistant to noise, let's gooo

--- perception_pkg/setup.py
+++ perception_pkg/setup.py
@@ -16,7 +16,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
 
         (os.path.join('share', package_name, 'models'),
-         glob(os.path.join('weights', '04.pth'))),
+         glob(os.path.join('weights', '05.pth'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
