f9967463a967e956386480fd2efaf6564ab7509e
maltesundgren <malte.sundgren@gmail.com>
2025-04-15 09:44:23 +0200

    changes in localization

--- icp_node/icp_node/icp_node.py
+++ icp_node/icp_node/icp_node.py
@@ -30,19 +30,10 @@ class ICP_node(Node):
         self.y = 0.0
 
         # Subscribe to lidar
-        self.create_subscription(LaserScan,'/scan',self.lidar_callback,1) # CHANGED
+        self.create_subscription(LaserScan,'/scan',self.lidar_callback,1)
 
         # Initialize subscriber pose from update step of EKF
-        self.create_subscription(PoseWithCovarianceStamped, '/localization/dead_reckoning_position', self.localization_pose_cb, 10)
-
-        # Initialize publisher that drive_2 can subscribe to
-        # self.have_new_transform_pub = self.create_publisher(TransformStamped, 'ICP/have_new_transform', 1)
-
-        # Initialize subscriber to brain
-        # self.create_subscription(Bool, '/icp_node/get_get_new_reference_scan', self.get_new_ref_scan_cb, 1)
-
-        # Initialize publiher to brain
-        # self.have_scan_pub = self.create_publisher(Bool, '/icp_node/have_scan', 1)
+        self.create_subscription(PoseWithCovarianceStamped, '/localization/dead_reckoning_position', self.localization_pose_cb, 1)
 
         # Initialize the transform buffer§
         self.tf_buffer = Buffer()
@@ -67,12 +58,11 @@ class ICP_node(Node):
 
         # intialize attribute for transform
         self.t = None
-        self.t_mat_old = None
-        self.outlier_margin = 0.5
+        self.t_mat = None
 
         # Initialize counters
-        self.counter = 0
-        self.N = 10
+        # self.counter = 0
+        # self.N = 10
         self.bad_scan_counter  = 0
         self.threshold_new_scan = 5
 
@@ -153,75 +143,114 @@ class ICP_node(Node):
         # Transform point cloud to odom
         cloud_odom = do_transform_cloud(cloud, t)
 
-        # # Transform point cloud to map
-        # transformed_cloud = do_transform_cloud(cloud_odom, self.t)
+        # Transform point cloud to map
+        transformed_cloud = do_transform_cloud(cloud_odom, self.t)
 
         # Transform to open3d format
-        pointcloud = self.create_open3d_pointcloud(cloud_odom)
+        pointcloud = self.create_open3d_pointcloud(transformed_cloud)
 
         # get new reference scan if bool is true, this is the case in the origin and when set by the brain
         if self.get_new_reference_scan:
             self.get_new_reference_scan = False
-            pos = (self.x,self.y)
+            # pos = (self.x,self.y)
             # self.get_logger().info(f'Position from localization when getting new reference scan; x: {self.x}, y: {self.y}')
 
             # append position of robot for the pointcloud and the pointcloud transformed to odom frame
-            self.reference_clouds.append((pos, pointcloud))
-
-            # publish message that new scan is added to brain
-            # bool_msg = Bool()
-            # bool_msg.data = True
-            # self.have_scan_pub.publish(bool_msg)
+            # self.reference_clouds.append((pos, pointcloud))
+            self.reference_clouds.append(pointcloud)
 
             # set reference cloud
-            self.ref_cloud = pointcloud
+            # self.ref_cloud = pointcloud
         
-        # if there is two or more reference scans in the list compare which one is closest and use that as reference cloud when doing ICP algorithm
-        if len(self.reference_clouds) >= 2:
-            self.get_logger().debug('Have more than one reference cloud')
-            min_distance = np.inf
-            for pos_cloud, cloud in self.reference_clouds:
-                distance = math.hypot(self.x-pos_cloud[0], self.y-pos_cloud[1])
-
-                if distance < min_distance:
-                    self.get_logger().debug('setting new reference cloud')
-                    self.ref_cloud = cloud
-
-        # if this it the first transform set the initial guess to be an indentity matrix, otherwise use the old transform as guess
-        if self.t_mat_old is None:
-            previous_transform = np.identity(4)
-        else:
-            previous_transform = self.t_mat_old
-
-        # Do ICP algorithm
-        result_icp = registration_icp(source=pointcloud,
-                        target=self.ref_cloud,
-                        max_correspondence_distance= 0.10,
-                        init=previous_transform, # The algorithm start with this and then tries to optimize for a better transform for map-odom
+        no_good_icp = True
+        # self.get_logger().info('-----------------Looking for good ICP result---------------')
+        if len(self.reference_clouds)>0:
+            # self.get_logger().info(f'number of reference clouds: {len(self.reference_clouds)}')
+            # iter = 1
+            best_overlap = 0
+            for ref_cloud in self.reference_clouds:
+                # Do ICP algorithm for the current reference cloud
+                result_icp = registration_icp(source=pointcloud,
+                        target=ref_cloud,
+                        max_correspondence_distance= 0.05,
+                        init=np.identity(4), 
                         estimation_method=TransformationEstimationPointToPoint(),
                         criteria=ICPConvergenceCriteria(relative_fitness=1e-6, relative_rmse=1e-6, max_iteration=1000)
                         )
+                # self.get_logger().info(f'iter: {iter}, overlap: {result_icp.fitness}, rmse: {result_icp.inlier_rmse}')
+                
+                if result_icp.fitness >= 0.25 and result_icp.inlier_rmse < 0.1:
+                    t_mat = result_icp.transformation.copy()
+                    new_x_y = np.array([t_mat[0, 3], t_mat[1, 3]])
+                    if np.linalg.norm(new_x_y) < 0.10:
+                        # norm also okay so we have a good icp result
+                        no_good_icp = False
+                        if result_icp.fitness > best_overlap:
+                            # self.get_logger().info(f'Taking reference cloud from iteration: {iter}')
+                            # copy result from icp, have to copy to be able to write to it
+                            transform_matrix = t_mat
+                    # else:
+                        # self.get_logger().info(f'Too high norm for reference cloud in iteration: {iter}')
+                # iter +=1
 
-        # Checking overlap and inlier rmse to make sure transform is good to use
-        overlap = result_icp.fitness 
-        rmse = result_icp.inlier_rmse
-        # self.get_logger().info(f'fitness: {overlap}, rmse: {rmse}')
+        # # if there is two or more reference scans in the list compare which one is closest and use that as reference cloud when doing ICP algorithm
+        # if len(self.reference_clouds) >= 2:
+        #     self.get_logger().debug('Have more than one reference cloud')
+        #     min_distance = np.inf
+        #     for pos_cloud, cloud in self.reference_clouds:
+        #         distance = math.hypot(self.x-pos_cloud[0], self.y-pos_cloud[1])
 
-        if overlap < 0.3 or rmse > 0.1:
-            # self.get_logger().info(f'Not good enough conditions, fitness: {overlap}, rmse: {rmse}')
+        #         if distance < min_distance:
+        #             self.get_logger().debug('setting new reference cloud')
+        #             self.ref_cloud = cloud
+
+        # # if this it the first transform set the initial guess to be an indentity matrix, otherwise use the old transform as guess
+        # if self.t_mat_old is None:
+        #     previous_transform = np.identity(4)
+        # else:
+        #     previous_transform = self.t_mat_old
+
+        # # Do ICP algorithm
+        # result_icp = registration_icp(source=pointcloud,
+        #                 target=self.ref_cloud,
+        #                 max_correspondence_distance= 0.10,
+        #                 init=previous_transform, # The algorithm start with this and then tries to optimize for a better transform for map-odom
+        #                 estimation_method=TransformationEstimationPointToPoint(),
+        #                 criteria=ICPConvergenceCriteria(relative_fitness=1e-6, relative_rmse=1e-6, max_iteration=1000)
+        #                 )
+
+        # # Checking overlap and inlier rmse to make sure transform is good to use
+        # overlap = result_icp.fitness 
+        # rmse = result_icp.inlier_rmse
+        # # self.get_logger().info(f'fitness: {overlap}, rmse: {rmse}')
+
+        # if overlap < 0.3 or rmse > 0.1:
+        #     # self.get_logger().info(f'Not good enough conditions, fitness: {overlap}, rmse: {rmse}')
+        #     self.t.header.stamp = msg.header.stamp
+        #     self._tf_broadcaster.sendTransform(self.t)
+
+        #     self.bad_scan_counter += 1
+
+        #     if self.bad_scan_counter >= self.threshold_new_scan:
+        #         self.get_logger().info(f'{self.threshold_new_scan} insufficent scans in a row, getting new reference scan')
+        #         # self.get_new_reference_scan = True 
+        #         self.bad_scan_counter = 0 # reset counter
+        #     return
+
+        # if no good icp result was found increment bad scans and send the latest transform
+        if no_good_icp:
             self.t.header.stamp = msg.header.stamp
             self._tf_broadcaster.sendTransform(self.t)
-
             self.bad_scan_counter += 1
-
             if self.bad_scan_counter >= self.threshold_new_scan:
                 self.get_logger().info(f'{self.threshold_new_scan} insufficent scans in a row, getting new reference scan')
-                # self.get_new_reference_scan = True 
+                self.get_new_reference_scan = True 
                 self.bad_scan_counter = 0 # reset counter
             return
 
-        # copy result from icp, have to copy to be able to write to it
-        transform_matrix = result_icp.transformation.copy()
+
+        # # copy result from icp, have to copy to be able to write to it
+        # transform_matrix = result_icp.transformation.copy()
 
         # Make sure that we are only operating in the xy-plane
         transform_matrix[2,3] = 0.0 # zero out translation in z
@@ -229,15 +258,15 @@ class ICP_node(Node):
         transform_matrix[:3, 2] = [0.0, 0.0, 1.0] 
         transform_matrix[2, :3] = [0.0, 0.0, 1.0]
 
-        # Compare with previous translation in transform so that the new transform will not move robot too much
-        comp_transforms = np.array([[self.t.transform.translation.x - transform_matrix[0, 3]], 
-                                    [self.t.transform.translation.y - transform_matrix[1, 3]]])
+        # # Compare with previous translation in transform so that the new transform will not move robot too much
+        # comp_transforms = np.array([[self.t.transform.translation.x - transform_matrix[0, 3]], 
+        #                             [self.t.transform.translation.y - transform_matrix[1, 3]]])
 
-        if np.linalg.norm(comp_transforms) > 0.1:
-            # self.get_logger().info(f'The norm was too high: {np.linalg.norm(comp_transforms)}')
-            self.t.header.stamp = msg.header.stamp
-            self._tf_broadcaster.sendTransform(self.t)
-            return 
+        # if np.linalg.norm(comp_transforms) > 0.1:
+        #     # self.get_logger().info(f'The norm was too high: {np.linalg.norm(comp_transforms)}')
+        #     self.t.header.stamp = msg.header.stamp
+        #     self._tf_broadcaster.sendTransform(self.t)
+        #     return 
 
         # reset bad scan counter so that it is only when multiple scans are bad in a row that triggers a new reference scan
         self.bad_scan_counter = 0
@@ -245,9 +274,12 @@ class ICP_node(Node):
         self.get_logger().debug('Updating map-odom tf')
         
         # reset counter since an update is going to be processed
-        self.counter = self.N
+        # self.counter = self.N
 
-        self.t_mat_old = transform_matrix
+        if self.t_mat is None:
+            self.t_mat = transform_matrix
+        else:
+            self.t_mat = transform_matrix @ self.t_mat # increment transform matrix with previous transform matrix
 
         # Update map and odom transform with result from ICP
         self.t.header.frame_id = "map"
@@ -255,12 +287,12 @@ class ICP_node(Node):
         self.t.header.stamp = msg.header.stamp
 
         # Extract translation from transformation matrix
-        self.t.transform.translation.x = transform_matrix[0, 3]
-        self.t.transform.translation.y = transform_matrix[1, 3]
+        self.t.transform.translation.x = self.t_mat[0, 3]
+        self.t.transform.translation.y = self.t_mat[1, 3]
         self.t.transform.translation.z = 0.0 # again make sure that translation in z is zero
 
         # Extract rotation as quaternion
-        quat = quaternion_from_matrix(transform_matrix)
+        quat = quaternion_from_matrix(self.t_mat)
         self.t.transform.rotation.x = 0.0 # rotation around x should not happen so set it to zero
         self.t.transform.rotation.y = 0.0 # rotation around y should not happen so set it to zero
         self.t.transform.rotation.z = quat[2]
